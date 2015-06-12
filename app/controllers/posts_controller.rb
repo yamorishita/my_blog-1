2db50616669b66f452a0fab8f5e3dcce156252ee
Daisuke Nashiro <nashirox@gmail.com>
2015-06-12 18:36:25 +0900

    コントローラー実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,43 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.new(post_params)
+    @post.update_attributes(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/posts"
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category)
+  end
+
 end
